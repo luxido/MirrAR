278f82703fc317528e7d45c1fe70c6685314164d
Mordag <alexander.eggers91@gmail.com>
2015-12-08 16:00:41 +0100

    Included more gui files

---
+++ Assets/GUI/MenuEventController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuEventController : MonoBehaviour {
+
+    public void clickPlay()
+    {
+        Debug.Log("test");
+    }
+
+    public void clickExit()
+    {
+        Application.Quit();
+    }
+}
