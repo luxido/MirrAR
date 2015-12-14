3c60840fbfd44f78b1776a6f93383bb8db73dda4
Mordag <alexander.eggers91@gmail.com>
2015-12-14 20:44:05 +0100

    Minor changes

--- Assets/GUI/LevelEventController.cs
+++ Assets/GUI/LevelEventController.cs
@@ -1,7 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class LevelEventController : MonoBehaviour {
+
+    private bool createBuildMenu;
+
+    void OnGUI()
+    {
+        if (createBuildMenu)
+        {
+            createBuildMenu = false;
+            Debug.Log("TEST");
+
+            var panel = GameObject.Find("BuildMenu");
+            GameObject buttonTest = new GameObject("ButtonTest");
+
+            var image = buttonTest.AddComponent<Image>();
+            image.transform.parent = panel.transform;
+            image.rectTransform.sizeDelta = new Vector2(180, 50);
+            image.rectTransform.anchoredPosition = Vector3.zero;
+            image.color = new Color(1f, .3f, .3f, .5f);
+
+            var button = buttonTest.AddComponent<Button>();
+            button.targetGraphic = image;
+            button.onClick.AddListener(() => Debug.Log(Time.time));
+        }
+    }
 
 	public void clickLevel(string level)
     {
@@ -9,6 +34,7 @@ public class LevelEventController : MonoBehaviour {
         {
             case "Level 1":
                 Debug.Log("Case 1");
+                createBuildMenu = true;
                 break;
         }
     }
