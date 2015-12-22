355161971b41ec9d159a1dda50b5c21e70eb1408
Mordag <alexander.eggers91@gmail.com>
2015-12-22 16:12:42 +0100

    Implemented game menu and minor changes

---
+++ Assets/Source/GUI/BuildEventController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildEventController : MonoBehaviour {
+
+	public void backClick()
+    {
+        Debug.Log("back");
+    }
+}
---
+++ Assets/Source/GUI/GameEventController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameEventController : MonoBehaviour {
+
+	public void menuClick()
+    {
+        Debug.Log("Menu Click");
+    }
+
+    public void buildClick()
+    {
+        Debug.Log("Build Click");
+    }
+}
