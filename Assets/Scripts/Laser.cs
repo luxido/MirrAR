67ac31697d6375348532cd5a634880cb56b3e30b
lxlph <xuanlinh.do@haw-hamburg.de>
2015-12-29 20:48:29 +0100

    Laser reflektiert

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -6,31 +6,44 @@ public class Laser : MonoBehaviour {
     LineRenderer lineRenderer;
     GameObject target;
     GameObject generator;
-    public float linewidth = 10.0f;
-    public Vector3 directionToGo = new Vector3(0, 0, 100);
+    private float linewidth = 10.0f;
+    public Vector3 directionToGo;
+    public int lastLaserNumber;
+    public Vector3 startPos;
 
-
-    void Start() {
+    public void Start() {
         target = GameObject.Find("TargetPrefab");
         generator = GameObject.Find("LaserGeneratorPrefab");
         lineRenderer = GetComponent<LineRenderer>();
-        lineRenderer.SetVertexCount(2);
+        startPos = transform.position;
+        directionToGo = new Vector3(0, 0, 100);
+        int lastLaserNumber = 1;
         lineRenderer.SetWidth(linewidth, linewidth);
 
     }
 
-    void Update() {
-        
+void Update() {
+        drawLaserLine();
+
+    }
+
+    public void drawLaserLine() {
         RaycastHit hit;
         Vector3 direction = transform.TransformDirection(directionToGo);
-        if (Physics.Raycast(transform.position, direction, out hit, Mathf.Infinity)){
-            lineRenderer.SetPosition(0, transform.position);
-            lineRenderer.SetPosition(1, hit.point);
+        if (Physics.Raycast(transform.position, direction, out hit, Mathf.Infinity)) {
+            lineRenderer.SetPosition(lastLaserNumber - 1, startPos);
+            lineRenderer.SetPosition(lastLaserNumber, hit.point);
+            if ((hit.collider.tag == "Mirror")) {
+                lastLaserNumber++;
+                lineRenderer.SetVertexCount(lastLaserNumber + 1);
+                directionToGo = Vector3.Reflect((hit.point - startPos).normalized, hit.normal);
+                startPos = hit.point;
+                drawLaserLine();
+            }
         }
-
         /*
             if (!(hit.transform.gameObject.layer == LayerMask.NameToLayer("Mirror"))) {
-
+            //
         }*/
     }
 }
