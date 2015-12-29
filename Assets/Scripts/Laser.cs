8b032b531b7ac9c7cd8b55ab72ae5ab3e1534666
lxlph <xuanlinh.do@haw-hamburg.de>
2015-12-29 13:18:28 +0100

    Laserimplementierung

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -1,80 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Laser : MonoBehaviour {
-    private LineRenderer lineRenderer;
-    private float counter;
-    private float dist;
-    private float laserWidth = 10.0f;
-    public float maxLength = 50.0f;
-
-    public Transform origin;
-    public Transform destination;
-    private Transform myTransform;
-    private Vector3 offset;
-    private Vector3[] position;
-    private int length;
-    public float noise = 1.0f;
-
-    public float lineDrawSpeed = 6f;
+    LineRenderer lineRenderer;
+    GameObject target;
+    GameObject generator;
+    public float linewidth = 10.0f;
+    public Vector3 directionToGo = new Vector3(0, 0, 100);
 
 
-    // Use this for initialization
     void Start() {
+        target = GameObject.Find("TargetPrefab");
+        generator = GameObject.Find("LaserGeneratorPrefab");
         lineRenderer = GetComponent<LineRenderer>();
-        //lineRenderer.SetPosition(0, origin.position);
-        lineRenderer.SetWidth(laserWidth, laserWidth);
-        myTransform = transform;
-        offset = new Vector3(0, 0, 0);
+        lineRenderer.SetVertexCount(2);
+        lineRenderer.SetWidth(linewidth, linewidth);
 
-        //dist = Vector3.Distance(origin.position, destination.position);
     }
 
     void Update() {
-        UpdateLength();
-
-        for (int i = 0; i < length; i++) {
-            //Set the position here to the current location and project it in the forward direction of the object it is attached to
-            offset.x = myTransform.position.x + i * myTransform.forward.x + Random.Range(-noise, noise);
-            offset.z = i * myTransform.forward.z + Random.Range(-noise, noise) + myTransform.position.z;
-            position[i] = offset;
-            position[0] = myTransform.position;
-
-            lineRenderer.SetPosition(i, position[i]);
-
+        
+        RaycastHit hit;
+        Vector3 direction = transform.TransformDirection(directionToGo);
+        if (Physics.Raycast(transform.position, direction, out hit, Mathf.Infinity)){
+            lineRenderer.SetPosition(0, transform.position);
+            lineRenderer.SetPosition(1, hit.point);
         }
+
         /*
-        if (counter < dist) {
-            lineRenderer.SetWidth(laserWidth, laserWidth);
-            counter += .1f / lineDrawSpeed;
+            if (!(hit.transform.gameObject.layer == LayerMask.NameToLayer("Mirror"))) {
 
-            float x = Mathf.Lerp(0, 20, counter);
-
-            Vector3 pointA = origin.position;
-            Vector3 pointB = destination.position;
-
-            Vector3 pointALongLine = x * Vector3.Normalize(pointB - pointA) + pointA;
         }*/
     }
-
-    void UpdateLength() {
-        //Raycast from the location of the cube forwards
-        RaycastHit[] hit;
-        hit = Physics.RaycastAll(myTransform.position, myTransform.forward, maxLength);
-        int i = 0;
-        while (i < hit.Length) {
-            //Check to make sure we aren't hitting triggers but colliders
-            if (!hit[i].collider.isTrigger) {
-                length = (int)Mathf.Round(hit[i].distance) + 2;
-                position = new Vector3[length];
-                //Move our End Effect particle system to the hit point and start playing it
-                lineRenderer.SetVertexCount(length);
-                return;
-            }
-            i++;
-        }
-        length = (int)maxLength;
-        position = new Vector3[length];
-        lineRenderer.SetVertexCount(length);
-    }
 }
+
--- Assets/Scripts/Mirror.cs
+++ Assets/Scripts/Mirror.cs
@@ -7,31 +7,29 @@ public class Mirror : MonoBehaviour {
     GameObject obj;
 
     void Start() {
-        //Input.simulateMouseWithTouches = true;
-        
     }
+
     void Update() {
         if (Input.GetMouseButtonDown(0) && !createMirror) {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
-            //Vector3 touchPositionForPlacing = 
             if ((Physics.Raycast(ray, out hit) && hit.collider.gameObject.name == "Capsule")) {
-                //Debug.Log("get capsule");
                 createMirror = true;
             }
         }
 
-        if (Input.GetMouseButtonUp(0) && createMirror ) {
+        if (Input.GetMouseButtonUp(0) && createMirror) {
             Debug.Log("createMirror");
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
+
             if (Physics.Raycast(ray, out hit)) {
-                obj = Instantiate(Resources.Load("MirrorPrefab"), hit.point, transform.rotation) as GameObject;
+                Vector3 position = hit.point + new Vector3(0, 100, 0);
+                obj = Instantiate(Resources.Load("MirrorPrefab"), position, transform.rotation) as GameObject;
                 Transform targetTransform = GameObject.Find("ImageTarget").transform;
                 obj.transform.SetParent(targetTransform, true);
                 obj.transform.localScale = new Vector3(0.1f, 0.1f, 0.02f);
                 createMirror = false;
-                
             }
         }
     }
