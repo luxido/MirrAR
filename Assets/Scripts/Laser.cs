c82131ee29e707b67e73c1c818baa20d084a26d7
lxlph <xuanlinh.do@haw-hamburg.de>
2016-01-04 19:16:59 +0100

    Laser geht nun durch Target

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -2,7 +2,7 @@
 using UnityEngine.UI;
 
 public class Laser : MonoBehaviour {
-    LineRenderer [] lineRenderer;
+    LineRenderer[] lineRenderer;
     GameObject generator;
     GameObject[] target;
     public static int targetAmount;
@@ -16,7 +16,7 @@ public class Laser : MonoBehaviour {
         lineRenderer = new LineRenderer[10];
         //Falls es noch alte Lines gibt, diese löschen
         for (int i = 0; i < lineRenderer.Length; i++) {
-            if (GameObject.Find("Line " + i) !=null) {
+            if (GameObject.Find("Line " + i) != null) {
                 DestroyImmediate(GameObject.Find("Line " + i));
             }
             lineRenderer[i] = new GameObject("Line " + i).AddComponent<LineRenderer>();
@@ -39,53 +39,56 @@ public class Laser : MonoBehaviour {
         Vector3 direction = transform.TransformDirection(directionToGo);
         //Erstmal abfragen, ob es nur ein Laserstrahl gibt, damit die positions nicht immer neu gesetzt werden
         if (Physics.Raycast(transform.position, direction, out hit, Mathf.Infinity) && lastLaserNumber == 1) {
-            lineRenderer[0].SetWidth(linewidth, linewidth); 
+            lineRenderer[0].SetWidth(linewidth, linewidth);
             lineRenderer[0].SetPosition(0, transform.position);
             lineRenderer[0].SetPosition(1, hit.point);
+            positions[1] = hit.point;
+            positions[0] = transform.position;
             //Wurde das Ziel erreicht?
-            if (hit.collider.tag =="Target") {
-                Debug.Log("You Win");
-            }
-            //Wenn ein Mirror getroffen wird, gibt es ein Laserstrahl mehr
-            else if (hit.collider.tag == "Mirror") {
+            if (hit.collider.tag == "Target" || hit.collider.tag == "Mirror") {
                 lastLaserNumber++;
-                positions[1] = hit.point;
-                positions[0] = transform.position;
                 for (int i = 1; i < lastLaserNumber; i++) {
-                    Debug.Log("position " + i + ": " + positions[i]);
-                    directionToGo = Vector3.Reflect((positions[i] - positions[i - 1]).normalized, hit.normal);
-                    direction = transform.TransformDirection(directionToGo);
+                    //Debug.Log("position " + i + ": " + positions[i]);
+                    //Wenn ein Mirror getroffen wird, gibt es ein Laserstrahl mehr
+                    if (hit.collider.tag == "Target") {
+                        Debug.Log("Target Hit");
+                    } else if (hit.collider.tag == "Mirror") {
+                        directionToGo = Vector3.Reflect((positions[i] - positions[i - 1]).normalized, hit.normal);
+                        direction = transform.TransformDirection(directionToGo);
+                    }
                     if (Physics.Raycast(positions[i], direction, out hit, Mathf.Infinity)) {
                         lineRenderer[i].SetPosition(0, positions[i]);
                         lineRenderer[i].SetPosition(1, hit.point);
                         //Wurde das Ziel getroffen?
                         if (hit.collider.tag == "Target") {
                             Debug.Log("You Win");
+                            positions[lastLaserNumber] = hit.point;
+                            lastLaserNumber++;
                         }
                         //Wenn ein Mirror getroffen wird, gibt es ein Laserstrahl mehr
-                        else if ((hit.collider.tag == "Mirror")) {   
+                        else if ((hit.collider.tag == "Mirror")) {
                             positions[lastLaserNumber] = hit.point;
                             lastLaserNumber++;
                         }
                     }
                 }
+
             }
+
         }
 
         laserHitEnd();
     }
 
-    public void initTargets(int amount)
-    {
+    public void initTargets(int amount) {
         targetAmount = amount;
         target = new GameObject[amount];
     }
 
-    void laserHitEnd()
-    {
+    void laserHitEnd() {
         //Überprüfen ob der Laser alle Targets trifft.
         //Überprüfen ob es ein weiteres Level gibt. Wenn nicht muss der "Next Level" Button deaktiviert werden.
-        
+
         //Mirror.nextLevelButton.GetComponent<Button>().interactable = false; um den Next Level Button zu deaktivieren
     }
 }
